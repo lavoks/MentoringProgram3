fe5f3edf6670fd8a319246527129cd2f956dd173
Oksana Lavrovych <oksana_lavrovych@epam.com>
2019-06-12 14:28:28 +0300

    Added gitignore file

--- src/com/homeTaskLecture4/Testing.java
+++ src/com/homeTaskLecture4/Testing.java
@@ -15,6 +15,11 @@ public class Testing {
         System.out.println(13 / 13);
         System.out.println(14 / 13);
 
+        System.out.println(0 % 13);
+        System.out.println(1 % 13);
+        System.out.println(2 % 13);
+        System.out.println(3 % 13);
+
 
 
     }
