0e0a899c4aa0aefc78abae27744f47e485b2972c
Oksana_Lavrovych <Lavoks8891@yahoo.com>
2020-03-03 18:26:49 +0200

    Added module 5 triangle solution + Exception2

--- src/com/module5Homework/triangle/TestTriangle.java
+++ src/com/module5Homework/triangle/TestTriangle.java
@@ -1,10 +1,10 @@
 package com.module5Homework.triangle;
 
-public class TriangleDescribtion {
+public class TestTriangle {
     public static void main(String[] args) {
         Point A = new Point(1, 1);
-        Point B = new Point(2,2);
-        Point C = new Point(3,2);
+        Point B = new Point(3,1);
+        Point C = new Point(3,3);
         try {
             System.out.println("Area " + TriangleSquareFormula.triangleSquare(A, B, C));
         } catch (TriangleExeption triangleExeption) {
--- src/com/module5Homework/triangle/TriangleSquareFormula.java
+++ src/com/module5Homework/triangle/TriangleSquareFormula.java
@@ -3,10 +3,10 @@ package com.module5Homework.triangle;
 public class TriangleSquareFormula {
 
     public static double triangleSquare(Point A, Point B, Point C) throws TriangleExeption {
-        if(true){
+        double triangleSquare = ((A.x * B.y +B.x*C.y +C.x*A.y)-(A.y*B.x+B.y*C.x+C.y*A.x)) / 2;
+        if(triangleSquare==0){
             throw new TriangleExeption("errrrrrrrrrrrrrrrrr");
         }
-        double triangleSquare = (A.x * (B.y - C.y) + B.x * (C.y - A.y) + C.x * (A.y - B.y)) / 2;
         return triangleSquare;
     }
 
